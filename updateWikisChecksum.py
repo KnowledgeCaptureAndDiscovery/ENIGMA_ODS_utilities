78b8aaf2fe91512602049b8b11ffde7c5aeff857
Hernán Vargas <hern.var@gmail.com>
2021-06-17 12:12:59 -0400

    allow only one hash per page

--- updateWikisChecksum.py
+++ updateWikisChecksum.py
@@ -1,22 +1,25 @@
-import hashlib
-import json
-import requests
-import pandas as pd
+import hashlib, json, requests, re
 
 endpoints = [
     {
         "wiki": {
-            "url": "http://organicdatacuration.org/enigma_dev/api.php",
-            "username": "_YOUR_BOT_",
-            "password": "_BOT_PASSWORD_"
+            "url": "",
+            "username": "",
+            "password": ""
         },
         "sparql": {
-            "url": "_SPARQL_ENDPOINT_",
+            "url": "",
             "username": "",
             "password": ""
         }
     },
 ]
+
+hashRegex = r'Checksum \(E\)=[a-zA-Z0-9]{40}'
+emptySet = r'\{\{\#set:\|?\}\}'
+doubleOr = r'\|\s*\|'
+exServ = "organicdatacuration.org"
+inServ = "localhost:8080"
 
 class SessionWithUrlBase(requests.Session):
     def __init__(self, url_base=None, *args, **kwargs):
@@ -34,18 +37,29 @@ def check_all_files (generalConf):
     botSession = createWikiBotSession(generalConf["wiki"])
 
     tkn = getCSRFToken(botSession)
-    updateChecksum = getUpdateChecksumF(botSession, tkn)
+    updatePage = getUpdatePageF(botSession, tkn)
 
-    wikiEntries = getAllFiles(sparqlSession)
+    wikiEntries = getAllFiles(sparqlSession, generalConf["wiki"]["url"])
+    i = 0
     for entry in wikiEntries:
+        #print("page: " + entry["page"] + "\tfile: " + entry["file"] + "\thash: " + (entry["hash"] if (entry["hash"]) else ""))
+        if not "images" in entry["file"]:
+            print("Warning! this page: " + entry["page"] + " has an invalid reference!")
+            continue
         nHash = getExternalFileHash(entry["file"])
         if (nHash != entry["hash"]):
             pagename = entry["page"].split("/")[-1]
             print(pagename + " has a new hash: " + nHash)
-            updateChecksum(pagename, nHash)
+            content = getWikiContent(botSession, pagename)
+            newContent = re.sub(hashRegex, '', content)
+            newContent = re.sub(emptySet, '', newContent)
+            newContent = re.sub(doubleOr, '|', newContent)
+            newContent += "\n{{#set:|Checksum (E)="+nHash+"}}"
+            newContent = re.sub(r'\n+', '\n', newContent)
+            #print(content + "\n will be replaces with\n" + newContent)
+            updatePage(pagename, newContent)
         else:
             print(entry["page"] + " old hash is still valid")
-        break
 
 def createSparqlSession (conf):
     sparqlSession = SessionWithUrlBase(url_base=conf["url"])
@@ -53,7 +67,7 @@ def createSparqlSession (conf):
     return sparqlSession
 
 def createWikiBotSession (conf):
-    botSession = SessionWithUrlBase(url_base=conf["url"])
+    botSession = SessionWithUrlBase(url_base=conf["url"] + "/api.php")
     # First we need to retrieve login token
     PARAMS_TOKEN = {
         'action':"query",
@@ -63,7 +77,6 @@ def createWikiBotSession (conf):
     }
     loginTokenRes = botSession.get("", params=PARAMS_TOKEN).json()
     LOGIN_TOKEN = loginTokenRes['query']['tokens']['logintoken']
-    print("Login Token: ",LOGIN_TOKEN)
 
     # Go to http://organicdatacuration.org/enigma_new/index.php/Special:BotPasswords for lgname & lgpassword, and add them below
     PARAMS_LOGIN = {
@@ -75,26 +88,29 @@ def createWikiBotSession (conf):
     }
 
     loginReq = botSession.post("", data=PARAMS_LOGIN).json()
-    print(loginReq)
     return botSession
 
-def getAllFiles (sparql):
-    query = "PREFIX enigma: <https://w3id.org/enigma#>"\
+def getAllFiles (sparql, wikiurl):
+    query = "PREFIX wiki: <" + wikiurl.replace(exServ, inServ) + "/index.php/Special:URIResolver/> "\
           + "SELECT ?page ?file ?hash WHERE {"\
-          + "  ?page enigma:hasContentUrl ?file ."\
-          + "  optional {?page enigma:checksum ?hash}"\
+          + "  ?page wiki:Property-3AHasContentUrl_-28E-29 ?file ."\
+          + "  optional {?page wiki:Property-3AChecksum_-28E-29 ?hash}"\
           + "} ORDER BY ?page"
 
     response = sparql.post("", data = {'query': query})
     res = json.loads(response.text)
 
     query_results=[]
+    revised_pages = set()
     for item in res['results']['bindings']:
         entry = {} 
-        entry["page"] = item["page"]["value"] if ("page" in item) else None
-        entry["file"] = item["file"]["value"] if ("file" in item) else None
-        entry["hash"] = item["hash"]["value"] if ("hash" in item) else None
-        query_results.append(entry)
+        pg = item["page"]["value"] if ("page" in item) else None
+        if not pg in revised_pages:
+            revised_pages.add(pg)
+            entry["page"] = pg
+            entry["file"] = item["file"]["value"] if ("file" in item) else None
+            entry["hash"] = item["hash"]["value"] if ("hash" in item) else None
+            query_results.append(entry)
     
     return query_results
 
@@ -124,21 +140,34 @@ def getCSRFToken (session):
     DATA = response.json()
     return DATA['query']['tokens']['csrftoken']
 
-def getUpdateChecksumF (session, token):
+def getWikiContent (session, page):
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "prop": "revisions",
+        "titles": page,
+        "rvprop": "content"
+    }
+    response = session.get("", params=PARAMS)
+    DATA = response.json()
+    pages = DATA["query"]["pages"]
+    for page in pages:
+        return pages[page]["revisions"][0]["*"]
+
+def getUpdatePageF (session, token):
     def _getEditParams(page, append):
         return {
             "action": "edit",
             "title": page,
-            "section": "new",
+            "section": "0",
             "format": "json",
             "text": append,
             "token": token
         }
-    def _updateChecksum(page, checksum):
-        text_to_append="{{#set:|Checksum (E)="+checksum+"}}" #FIXME: this ADDs a checksum, does not update!
-        req = session.post("", data=_getEditParams(page, text_to_append))
+    def _updatePage(page, content):
+        req = session.post("", data=_getEditParams(page, content))
         data = req.json()
         print(page + " updated!")
-    return _updateChecksum
+    return _updatePage
 
 check_all_files(endpoints[0])
